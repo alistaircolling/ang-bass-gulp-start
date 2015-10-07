cd5c005f07196b77b02d0745849208a95643e244
alistaircolling <alistair.colling@gmail.com>
2015-10-07 10:17:20 +0100

    loading json in rather than hard-coding into the routerapp

--- src/js/app.js
+++ src/js/app.js
@@ -81,27 +81,34 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
 //this is the controller that is defined above that is used in the 'about' state
 
-routerApp.controller('pricesController', function($scope) {
+//routerApp.controller('pricesController', function($scope) {
 
-    $scope.message = 'test';
+        //$scope.message = 'test';
 
-    $scope.rooms = [{
-        name: 'Basic',
-        price: 50,
-        image: 'http://www.designtripper.com/wp-content/uploads/2011/10/IMG_0202.jpg',
-        description: 'This is the cheapest room that we have.'
+        //$scope.rooms = [{
+            //name: 'Basic',
+            //price: 50,
+            //image: 'http://www.designtripper.com/wp-content/uploads/2011/10/IMG_0202.jpg',
+            //description: 'This is the cheapest room that we have.'
 
-    }, {
-        name: 'Pretty Fancy',
-        price: 100,
-        image: 'http://graphics8.nytimes.com/images/2011/04/05/t-magazine/05soho-casale/05soho-casale-custom1.jpg',
-        description: 'This is the mid-range suite.'
+        //}, {
+            //name: 'Pretty Fancy',
+            //price: 100,
+            //image: 'http://graphics8.nytimes.com/images/2011/04/05/t-magazine/05soho-casale/05soho-casale-custom1.jpg',
+            //description: 'This is the mid-range suite.'
 
-    },{
-        name: 'Executive Suite',
-        price: 200,
-        image: 'http://www.psstructures.com.au/wp-content/gallery/hospitality-presidential-suite/presidential-suite-3.jpg',
-        description: 'This is our best room.'
+        //}, {
+            //name: 'Executive Suite',
+            //price: 200,
+            //image: 'http://www.psstructures.com.au/wp-content/gallery/hospitality-presidential-suite/presidential-suite-3.jpg',
+            //description: 'This is our best room.'
 
-    }]
-})
+        //}]
+    /*})*/
+
+routerApp.controller('pricesController', function($scope, $http) {
+    $http.get('prices-data.json')
+        .then(function(res) {
+            $scope.rooms = res.data;
+        });
+});
